53394ca58a36bd7149a5df60ff4f2923558e4bd5
Deymison Elias dos Reis <newreis825@gmail.com>
2025-11-10 15:29:48 -0300

    Add model parameters for SVEM-GT v3.0

---
+++ 📁 model_params.py
@@ -0,0 +1,9 @@
+# SVEM-GT v3.0 model parameters (Ridge regression)
+INTERCEPT = 19.53
+
+COEFFS = {
+    "strain_pct": -1.27,
+    "doping": 0.85,
+    "quantum_geometry": 12.45,
+    "kappa": 5.11
+}
